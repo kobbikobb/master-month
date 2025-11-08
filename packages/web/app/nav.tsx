703d55bf0ade7c35cbe65efd2c65e985b2b896d4
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-11-08 19:31:32 +0000

    Add simple routing

---
+++ packages/web/app/nav.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router";
+
+export function Nav() {
+  return (
+    <nav>
+      <NavLink to="/" end>
+        Home
+      </NavLink>
+      <NavLink to="/goals" end>
+        Goals
+      </NavLink>
+    </nav>
+  );
+}
